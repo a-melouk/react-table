aa3b6ac042fb569c563798193710655b0a01a0b2
mabdou <abdoumelouk96@gmail.com>
2024-07-21 20:38:42 +0200

    Updated settings to publish component

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,37 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    //Specifies that the output of the build will be a library.
     lib: {
-      entry: path.resolve("src", "components/Table.tsx"),
-      name: "react-table-search-sort",
-      fileName: (format) => `react-array.${format}.js`,
+      //Defines the entry point for the library build. It resolves
+      //to src/index.ts,indicating that the library starts from this file.
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "react-jp-ui",
+      //A function that generates the output file
+      //name for different formats during the build
+      fileName: (format) => `index.${format}.js`,
     },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    //Generates sourcemaps for the built files,
+    //aiding in debugging.
+    sourcemap: true,
+    //Clears the output directory before building.
+    emptyOutDir: true,
   },
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.ts", // if you have any setup files,
-    include: ["src/**/*.test.tsx", "src/**/*.test.ts"],
-  },
+  //react() enables React support.
+  //dts() generates TypeScript declaration files (*.d.ts)
+  //during the build.
+  plugins: [react(), dts()],
 });
